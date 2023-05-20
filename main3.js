aacc856f7eeb54726b169e901fbe64f2c6e765c8
AdityaKumar5155 <adityakumar5155@gmail.com>
2023-05-20 15:06:26 +0530

    First

--- main3.js
+++ main3.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 let db;
 let users;
 async function connectToDB() {
-    db = await mongoose.connect('mongodb+srv://cyberKids:adisatpriya@cluster0.jeolobf.mongodb.net/quiz?retryWrites=true&w=majority');
+    db = await mongoose.connect('mongodb://127.0.0.1:27017/quiz')
     console.log('connected to database')
     const Schema = new mongoose.Schema({
         name: String,
@@ -11,13 +11,10 @@ async function connectToDB() {
         roll: Number,
         school: String
       });
-    users = await mongoose.model('users', Schema);
+    users = await mongoose.model('users', Schema)
     console.log('models connected')
 }
 connectToDB();
-
-
-
 
 const express = require('express')
 const socketIO = require('socket.io')
@@ -27,25 +24,34 @@ let server = http.createServer(app)
 let io = socketIO(server)
 app.use(express.static('public'))
 app.set('view-engine', 'ejs')
+app.set('socketio', io);
+
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
+// let res;
+app.post('/submitted', (req, res) => {
+    // res = res;
+    let io = req.app.get('socketio');
+    io.emit('hi!');
+    res.render('response.ejs')
+    // console.log(' redirect to /submitted')
+    // console.log( {name : res.body.name})
+})
+
+// let socket = io.app.get('socket');
+// console.log(socket);
+
 io.on('connection', (socket) => {
     console.log('connection successful');
     socket.on('submitted', (data) => {
         console.log(data);
-        users.create(data);
-        console.log('   Entry created successfully');
+        async function createUser(data){
+            await users.create(data);
+            console.log('Entry created successfully');
+        }
+        createUser(data);
     })
-})
-app.post('/submitted', (req, res) => {
-    console.log("Submitted successfully")
-    res.render('response.ejs')
-    console.log(' redirect to /submitted')
-    // console.log( {name : res.body.name})
-})
-app.get('/submitted', (req, res) => {
-    res.send("HELLO")
 })
 
 server.listen(4444)
---
+++ public/script.js
@@ -0,0 +1,17 @@
+let socket = io();
+
+function clicked(){
+    let name = $("#name").val();
+    let section = $("#section").val();
+    let clas = $("#class").val();
+    let roll = $("#roll").val();
+    let school = $("#school").val();
+    let send = {name:name,class:clas,section:section,roll:roll,school:school}
+    console.log(send)
+    socket.emit("submitted", send);
+}
+
+socket.on("hi!", () => {
+    console.log("Woooooooooohooooooooooooooooo!")
+})
+
